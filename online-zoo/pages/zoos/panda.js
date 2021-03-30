a44cd6f24e5db10a4756c806e0e99c7217a912f8
Balzamova <balzam0401@gmail.com>
2021-03-30 22:45:17 +0300

    feat: add donat and know blocks on zoos pages

---
+++ online-zoo/pages/zoos/panda.js
@@ -0,0 +1,28 @@
+/*
+.info-text {
+	font-family: Montserrat;
+	font-size: 18px;
+	line-height: 150%;
+	color: #000000;
+}
+
+.block-title {
+	font-family: Montserrat;
+	font-weight: 600;
+	font-size: 54px;
+	line-height: 66px;
+	text-transform: uppercase;
+	color: #00A092;
+}
+
+.img-arrow {
+	width: 24.28px;
+	height: 22px;
+	margin-left: 10px;
+}
+
+
+	transition: all 0.5s ease-in-out;
+
+
+*/
