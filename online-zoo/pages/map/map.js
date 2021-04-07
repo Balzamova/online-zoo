72f6402fc02a682205685f8e2728956bb502c1bd
Balzamova <balzam0401@gmail.com>
2021-04-07 21:38:59 +0300

    feat: add media for map page

---
+++ online-zoo/pages/map/map.js
@@ -0,0 +1,6 @@
+/* Burger Menu */
+document.querySelector('.header__burger').onclick = function () {
+  document.querySelector('.header__burger').classList.toggle('active');
+  document.querySelector('.header__menu').classList.toggle('active');
+	document.querySelector('body').classList.toggle('lock');
+}
