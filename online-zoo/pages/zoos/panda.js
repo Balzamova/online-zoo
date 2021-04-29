fea4cd90ed23cc526ca511888a0b39b3f4ee4839
Balzamova <balzam0401@gmail.com>
2021-04-29 21:28:11 +0300

    feat: add switch video func, slider video on zoos

--- online-zoo/pages/zoos/panda.js
+++ online-zoo/pages/zoos/panda.js
@@ -5,7 +5,7 @@ document.querySelector('.header__burger').onclick = function () {
 	document.querySelector('body').classList.toggle('lock');
 }
 
-/* Functions for toggle sidebar*/
+/* Toggle sidebar*/
 const arrowBtn = document.querySelector('.sidebar__live_arrow');
 const sidebar = document.querySelector('.sidebar');
 const sidebarLive = document.querySelector('.sidebar__live_block');
@@ -35,6 +35,21 @@ const toggleSidebar = () => {
 arrowBtn.addEventListener('click', toggleSidebar);
 sidebarOpenMain.addEventListener('click', toggleSidebar);
 
+/* Sidebar slider*/
+const sidebarList = document.querySelector('.sidebar__list');
+const sidebarItem = document.querySelectorAll('.sidebar__item');
+
+(function() {
+	sidebar.addEventListener('click', addCloneFirstLast);
+	
+	function addCloneFirstLast(e) {
+	  let parent = document.querySelector('.sidebar__list');
+	  let first = parent.querySelector('.sidebar__item');
+
+	  if (e.target.classList.contains('sidebar__arrow_item')) parent.appendChild(first);
+	};
+}).call(this);
+
 /* Input settings */
 
 const form = document.querySelector('.donat__donate');
@@ -52,3 +67,73 @@ donatInput.addEventListener('input', ()=> {
 		donatInput.value = value.slice(0, donatInput.maxLength);
 	}
 })
+
+/* Cameras slider */
+const sliderBlock = document.querySelector('.camera__more_content');
+const sliderItems = document.querySelector('.camera__more_container');
+const btnPrev = document.querySelector('.btn-left');
+const btnNext = document.querySelector('.btn-right');
+
+const sliderShift = 10;
+
+(function() {
+	sliderBlock.addEventListener('click', addCloneFirstLast);
+	
+	function addCloneFirstLast(e) {
+	  let parent = document.querySelector('.camera__more_container');
+	  let first = parent.querySelector('.camera__more_block');
+	  let last = parent.querySelector('.camera__more_block:last-child');
+
+	  if (e.target.classList.contains('btn-left')) parent.insertBefore(last, first);	  
+	  if (e.target.classList.contains('btn-right')) parent.appendChild(first);
+	};
+}).call(this);
+
+/* Switch main camera */
+
+const playBtns = document.querySelectorAll('.camera__more_play');
+
+playBtns.forEach(item => {
+	item.addEventListener('click', () => {
+		let currentCamera = item.parentElement.children[2].children[0];
+		let currentSrc = currentCamera.src;
+		let arrayCurrent = currentSrc.split('');
+		let currentPlaySrc = '';
+		
+		arrayCurrent.splice(arrayCurrent.length-1, 1, '1');
+		for (let i = 0; i < arrayCurrent.length; i++) {
+			currentPlaySrc += arrayCurrent[i];
+		}
+	
+		let bigCamera = document.querySelector('.camera__main');
+		let bigCameraSrc = bigCamera.src;
+		let arrayBig = bigCameraSrc.split('');
+		let bigCameraStopSrc = '';
+
+		arrayBig.splice(arrayBig.length-1, 1, '0');
+		for (let i = 0; i < arrayBig.length; i++) {
+			bigCameraStopSrc += arrayBig[i];
+		}
+
+		bigCamera.src = currentPlaySrc;
+		currentCamera.src = bigCameraStopSrc;
+	})
+});
+
+document.addEventListener("touchend", touch2Mouse, true);
+
+function touch2Mouse(e) {
+  var theTouch = e.changedTouches[0];
+  var mouseEv;
+
+  switch(e.type) {
+   	case "touchend":  mouseEv="click"; break;
+    default: return;
+  }
+
+  var mouseEvent = document.createEvent("MouseEvent");
+  mouseEvent.initMouseEvent(mouseEv, true, true, window, 1, theTouch.screenX, theTouch.screenY, theTouch.clientX, theTouch.clientY, false, false, false, false, 0, null);
+  theTouch.target.dispatchEvent(mouseEvent);
+
+  e.preventDefault();
+}
