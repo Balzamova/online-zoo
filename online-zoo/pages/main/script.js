8cea863db1f559d22a717411af92cc8b5c7fd7ff
Balzamova <balzam0401@gmail.com>
2021-05-04 12:40:55 +0300

    feat: add code for slider on pets page

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -22,3 +22,20 @@ donatInput.addEventListener('input', ()=> {
 		donatInput.value = value.slice(0, donatInput.maxLength);
 	}
 })
+
+/* Slider MEET SOME OUR PETS */
+
+const sliderBlock = document.querySelector('.meet-block__content');
+const sliderItems = document.querySelector('.meet-block__cardsholder');
+
+(function() {
+	sliderBlock.addEventListener('click', addCloneFirstLast);
+	
+	function addCloneFirstLast(e) {
+	  let first = sliderItems.querySelector('.meet-block__card');
+	  let last = sliderItems.querySelector('.meet-block__card:last-child');
+
+	  if (e.target.classList.contains('btn-left')) sliderItems.insertBefore(last, first);
+	  if (e.target.classList.contains('btn-right')) sliderItems.appendChild(first);
+	};
+}).call(this);
